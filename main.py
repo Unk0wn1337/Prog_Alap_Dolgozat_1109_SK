6327462f0f5af5878997ae1a094b5a9850cf938b
Unk0wn1337 <kelogames@gmail.com>
2023-11-09 11:23:16 +0100

    függvény meghivas elott sorszamozas

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 import Feladatok
-
-#Feladatok.elsoFeladat()
-#Feladatok.masodikFeladat()
-#Feladatok.harmadikFeladat("Ez az én dumám",4)
-#Feladatok.negyedikFeladat()
+# 1
+Feladatok.elsoFeladat()
+# 2
+Feladatok.masodikFeladat()
+# 3
+Feladatok.harmadikFeladat("Ez az én dumám",4)
+# 4
+Feladatok.negyedikFeladat()
+# 5
 Feladatok.otodikFeladat()
